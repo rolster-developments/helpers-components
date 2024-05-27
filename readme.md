112147eaa3996844f9d3bb9c53b7aa5e976545b1
ing.dacastillop <ing.dacastillop@gmail.com>
2024-05-27 07:23:56 -0500

    :tada: First commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Rolster Helpers Components
+
+Components utilities package compatible with Rolster projects.
+
+## Installation
+
+```
+npm i @rolster/helpers-components
+```
+
+## Configuration
+
+You must install the `@rolster/types` to define package data types, which are configured by adding them to the `files` property of the `tsconfig.json` file.
+
+```json
+{
+  "files": ["node_modules/@rolster/types/index.d.ts"]
+}
+```
+
+## Contributing
+
+- Daniel Andrés Castillo Pedroza :rocket:
---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+const plugins = [
+  commonjs(),
+  resolve(),
+  typescript({
+    tsconfig: './tsconfig.app.json',
+    declaration: true,
+    declarationDir: 'dist',
+    include: ['node_modules/@rolster/types/index.d.ts']
+  })
+];
+
+const rollupTs = (file) => {
+  return {
+    input: [`dist/esm/${file}.js`],
+    output: [
+      {
+        file: `dist/cjs/${file}.js`,
+        format: 'cjs',
+        sourcemap: true,
+        inlineDynamicImports: true
+      },
+      {
+        file: `dist/es/${file}.js`,
+        format: 'es',
+        sourcemap: true,
+        inlineDynamicImports: true
+      }
+    ],
+    plugins
+  };
+};
+
+const exports = ['index'];
+
+export default [...exports.map((file) => rollupTs(file))];
