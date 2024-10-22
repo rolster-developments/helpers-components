c1230a6dc61bb758468563e21b9d3b874601aa1d
ing.dacastillop <ing.dacastillop@gmail.com>
2024-10-22 09:15:05 -0500

    :sparkles: Implement new criteria in Project

--- src/shared.ts
+++ src/shared.ts
@@ -1,3 +1,13 @@
+import { hasPattern } from '@rolster/strings';
+
 export interface FilterCriteria<T = any> {
   apply(value: T): boolean;
+}
+
+export class PatternCriteria implements FilterCriteria {
+  constructor(private pattern: string) {}
+
+  public apply(value: any): boolean {
+    return hasPattern(JSON.stringify(value), this.pattern, true);
+  }
 }
